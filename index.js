f07adfc3ec60b6ba6ef661be5f66b4394795282f
Kseniia Kondrat <oootlichno@gmail.com>
2024-08-28 18:18:17 -0500

    finish

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const server = require('./api/server');
 
 const port = 9000;
 
+server.listen(9000, () => {
+    console.log('server started on http://localhost:9000')
+})
+
 // START YOUR SERVER HERE
